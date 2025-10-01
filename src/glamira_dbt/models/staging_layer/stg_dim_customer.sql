e89d1d0cdb55e3eec2e7b0b6b4e8c48eb1f018c5
ngoctan392k4 <ngoctan.392k4.toshiro@gmail.com>
2025-10-01 20:57:56 +0700

    remove utm source and utm media column

--- src/glamira_dbt/models/staging_layer/stg_dim_customer.sql
+++ src/glamira_dbt/models/staging_layer/stg_dim_customer.sql
@@ -8,9 +8,7 @@ SELECT DISTINCT
     COALESCE(NULLIF(dcs.email_address, ''), 'Not Defined') AS email_address,
     COALESCE(NULLIF(dcs.user_agent, ''), 'Not Defined') AS user_agent,
     COALESCE(NULLIF(dcs.user_id_db, ''), 'Not Defined') AS user_id_db,
-    COALESCE(NULLIF(dcs.resolution, ''), 'Not Defined') AS resolution,
-    COALESCE(NULLIF(dcs.utm_source, ''), 'Not Defined') AS utm_source,
-    COALESCE(NULLIF(dcs.utm_medium, ''), 'Not Defined') AS utm_medium
+    COALESCE(NULLIF(dcs.resolution, ''), 'Not Defined') AS resolution
 FROM dim_customer_source dcs
 
 UNION ALL
@@ -20,6 +18,4 @@ SELECT
     'Not Defined' AS email_address,
     'Not Defined' AS user_agent,
     'Not Defined' AS user_id_db,
-    'Not Defined' AS resolution,
-    'Not Defined' AS utm_source,
-    'Not Defined' AS utm_medium
+    'Not Defined' AS resolution
